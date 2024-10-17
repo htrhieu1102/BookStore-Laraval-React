be3e2bb98f664dc40c00da039689fa4d54eef3a0
HoangTrungHieu-20130260 <20130260@st.hcmuaf.edu.vn>
2024-10-18 01:44:09 +0700

    component create article

--- react/src/views/User.jsx
+++ react/src/views/User.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import { Tab, Nav, Row, Col } from 'react-bootstrap'
-
+import { FaRegFile } from "react-icons/fa";
 export const User = () => {
 	const currentYear = new Date().getFullYear();
+	const [image, setImage] = useState(null);
 
+	// Hàm xử lý khi người dùng chọn file
+	const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		if (file) {
+			const imageURL = URL.createObjectURL(file); // Tạo đường dẫn tạm cho ảnh
+			setImage(imageURL); // Cập nhật ảnh vào state
+		}
+	};
 	return (
 		<div className="main">
 			<div className="container">
-				<Tab.Container id="left-tabs-example" defaultActiveKey="first">
+				<Tab.Container id="left-tabs-example" defaultActiveKey="infor-user">
 					<Row>
 						<Col sm={3} className='box-menu'>
 							<div className="user-infor">
@@ -38,6 +48,42 @@ export const User = () => {
 						</Col>
 						<Col sm={9}>
 							<Tab.Content>
+								<Tab.Pane eventKey="create-article">
+									<p className="title-content">Đăng bài báo</p>
+									<form action="">
+										<div className="row">
+											<div className="col-6">
+												<p className="label">Tiêu đề:</p>
+												<input className='input' type="text" placeholder='Nhập...'/>
+												<p className="label mt-3">Miêu tả ngắn:</p>
+												<input className='input' type="text" placeholder='Nhập...' />
+											</div>
+											<div className="col-6 choose-file">
+												{image ? <img
+													className='img-fluid'
+													src={image}
+													alt="" />
+												: <img
+														className='img-fluid'
+														src="https://th.bing.com/th/id/R.ce69802fa9491f30d42e6a43e0cb1a39?rik=nWge2butlHUpWw&pid=ImgRaw&r=0"
+														alt="" />}
+											
+												
+												<input className='file' type="file" onChange={handleImageChange} />
+											</div>
+										</div>
+										<div className="row">
+											<div className="col-12">
+												<p className="label">Nội dung:</p>
+												<textarea className='input' type="text"placeholder='Nhập...' />
+											</div>
+											
+										</div>
+										<div className="box-button text-end">
+											<button className="button-send">Đăng bài</button>
+										</div>
+									</form>
+								</Tab.Pane>
 								<Tab.Pane eventKey="infor-user" className='change-information'>
 									<p className="title-content">Thông tin tài khoản</p>
 									<form action="">
