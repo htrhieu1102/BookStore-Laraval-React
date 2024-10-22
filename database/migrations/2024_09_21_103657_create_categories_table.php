8ed8eb57c76da8ff6b62eb1e278023bddb4ce6d2
HoangTrungHieu-20130260 <20130260@st.hcmuaf.edu.vn>
2024-10-22 14:59:19 +0700

    add col category db

--- database/migrations/2024_09_21_103657_create_categories_table.php
+++ database/migrations/2024_09_21_103657_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
             $table->boolean('active')->default(true);
             $table->timestamps();
